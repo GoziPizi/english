a4ca68365b02aab06ccd62a06755fe6b931a482c
gozipizi <quentin.gourdel@ensiie.fr>
2024-06-18 23:43:32 +0200

    Mentions légales

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { ContactFormComponent } from './contact-form/contact-form.component';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HeaderComponent, ContactFormComponent],
+  imports: [CommonModule, RouterOutlet, HeaderComponent, ContactFormComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
